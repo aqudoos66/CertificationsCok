5a7478affa74a7c2a7d4947628967da18aa061c6
Abdul Qudoos <168980623+aqudoos66@users.noreply.github.com>
2025-04-27 13:05:44 +0500

    Update sidebar.php

--- file/sidebar.php
+++ file/sidebar.php
@@ -1,40 +1,19 @@
-<div class="main-sidebar sidebar-style-2">
-        <aside id="sidebar-wrapper">
-          <div class="sidebar-brand">
-            <a href="index.php"> <img alt="image" src="assets/img/cok/logo.webp" class="header-logo" /> <span
-                class="logo-name">COK</span>
-            </a>
-          </div>
-          <ul class="sidebar-menu">
-            <!-- <li class="menu-header">Main</li> -->
-            <li class="dropdown active">
-              <a href="index.php" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="menu-toggle nav-link has-dropdown"><i
-                  data-feather="briefcase"></i><span>Certificates</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="certificate-form.php">Create</a></li>
-                <li><a class="nav-link" href="view-candidate.php">View</a></li>
-              </ul>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Course</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="chat.html">Add</a></li>
-                <li><a class="nav-link" href="portfolio.html">view</a></li>
-                
-              </ul>
-            </li>
-<!--             <li class="dropdown">
-              <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="mail"></i><span>Email</span></a>
-              <ul class="dropdown-menu">
-                <li><a class="nav-link" href="email-inbox.html">Inbox</a></li>
-                <li><a class="nav-link" href="email-compose.html">Compose</a></li>
-                <li><a class="nav-link" href="email-read.html">read</a></li>
-              </ul>
-            </li> -->
-            
-          </ul>
-        </aside>
-      </div>
+<ul class="sidebar-menu">
+  <li class="dropdown active">
+    <a href="index.php" class="nav-link"><i data-feather="monitor"></i><span>Dashboard</span></a>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="briefcase"></i><span>Certificates</span></a>
+    <ul class="dropdown-menu">
+      <li><a class="nav-link" href="certificate-form.php"><i data-feather="plus-circle"></i>Create</a></li>
+      <li><a class="nav-link" href="view-candidate.php"><i data-feather="eye"></i>View</a></li>
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a href="#" class="menu-toggle nav-link has-dropdown"><i data-feather="command"></i><span>Course</span></a>
+    <ul class="dropdown-menu">
+      <li><a class="nav-link" href="chat.html"><i data-feather="plus-circle"></i>Add</a></li>
+      <li><a class="nav-link" href="portfolio.html"><i data-feather="eye"></i>View</a></li>
+    </ul>
+  </li>
+</ul>
